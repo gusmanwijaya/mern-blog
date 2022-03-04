2165c552ced77be760f8e349db9832d40764e044
Gusman Wijaya <gusman.w.jaya@gmail.com>
2022-03-04 21:55:19 +0700

    Integrasi API di Home Page

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -1,23 +1,19 @@
 import React from "react";
 import "./blogitem.scss";
-import { RegisterBg } from "../../../assets";
 import { Button, Gap } from "../../../components";
 import { useNavigate } from "react-router-dom";
 
-const BlogItem = () => {
+const BlogItem = ({ image, title, author, body, date }) => {
   const navigate = useNavigate();
   return (
     <div className="blog-item">
-      <img src={RegisterBg} alt="Post" className="image-thumb" />
+      <img src={image} alt="Post" className="image-thumb" />
       <div className="content-detail">
-        <p className="title">Title Blog</p>
-        <p className="author">Author - Date Post</p>
-        <p className="body">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
-          fugit, optio iste sit reprehenderit ducimus ab aliquam molestias
-          laborum odio qui blanditiis, ipsam a aperiam modi eligendi delectus
-          sequi dolore?
+        <p className="title">{title}</p>
+        <p className="author">
+          {author} - {date}
         </p>
+        <p className="body">{body}</p>
         <Gap height={20} />
         <Button
           title="View Detail"
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  const [dataBlog, setDataBlog] = useState([]);
+
+  useEffect(() => {
+    Axios.get(`${process.env.REACT_APP_API_HOST}/blog/posts?perPage=2`)
+      .then((result) => setDataBlog(result.data.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="home-page-wrapper">
       <div className="create-wrapper">
@@ -16,10 +26,16 @@ const Home = () => {
       </div>
       <Gap height={20} />
       <div className="content-wrapper">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map((value, index) => (
+          <BlogItem
+            key={index}
+            image={`${process.env.REACT_APP_IMG_HOST}/${value?.image}`}
+            title={value?.title}
+            body={value?.body}
+            author={value?.author?.name}
+            date={value?.createdAt}
+          />
+        ))}
       </div>
       <div className="pagination">
         <Button type="button" title="Previous" />
